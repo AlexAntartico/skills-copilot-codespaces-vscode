ec8e3933f253571798e1515bb45fe9f74d1c81c9
Alex Garcia <ntarticpnguin@proton.me>
2024-06-16 20:39:27 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,18 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file. Use the fs module to read the file and send it to the client.
+
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+  fs.readFile('comments.html', 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
+}).listen(3000);
+console.log('Server listening on port 3000');
